f2cf4fa936535bd7df578e2974008ed484ccb59a
Eleonore <eleonore.caclard@gmail.com>
2021-07-18 16:31:12 -0400

    final version for each file

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+var questions = [{
+        title: "Which of the below is used in Javascript to insert special characters?",
+        choices: ["/", "&", "-", "%"],
+        answer: "/"
+    },
+
+    {
+        title: "How do you find the number with the greatest value of 'a' and 'b'?",
+        choices: ["Math.ceil(a,b)", "ceil(a,b)", "top(a,b)", "Math.max(a,b)"],
+        answer: "Math.max(a,b)"
+    },
+
+    {
+        title: "JavaScript ignores extra spaces",
+        choices : ["True", "False"],
+        answer: "True"
+    },
+
+    {
+        title: "What is the correct way to call the random method on the Math global object?",
+        choices: ["random.Math()", "Math(random)", "math.random()", "Math.random()"],
+        answer: "Math.random()"
+    },
+
+    {
+        title: "Which of the following functions is a valid function type that javascript supports?",
+        choices: ["A named function", "An Anonymous function", "1 and 2", "None of the above"],
+        answer: "1 and 2"
+    },
+
+    {
+        title: "How can you add a comment in a JavaScript code?",
+        choices: ["/*My JavaScript comment*/", "//My JavaScript comment", "#My JavaScript comment#", "//*#My JavaScript comment"],
+        answer: "//My JavaScript comment"
+    },
+
+    {
+        title: "Browsers that do not understand Javascript",
+        choices: ["Ignore all <script></script> and <noscript></noscript> tags", "Display the content of <script></script> tags", "Display the content of <script></script> and <noscript></noscript> tags", "Display the content of <noscript></noscript> tags"],
+        answer: "Display the content of <noscript></noscript> tags"
+    },
+
+    {
+        title: "What is the code to get a random number between 5 and 9 inclusive?",
+        choices: ["Math.floor((Math.random()*5+5)", "Math.floor((Math.random()*5+4)", "Math.floor((Math.random()*4+4)", "Math.floor((Math.random()*4+5)"],
+        answer: "Math.floor((Math.random()*5+5)"
+    },
+
+    {
+        title: "How many parameters can be passed to a function?",
+        choices: ["None", "As much as you want", "1 for each argument", "A maximum of 3"],
+        answer: "1 for each argument"
+    },
+
+    {
+        title: "How to round the number 15.2021 to a closer whole number?",
+        choices: ["Math.round(15.2021)", "Math.rnd(15.2021)", "Math.sin(15.2021)", "Math.sqrt(15.2021)"],
+        answer: "Math.round(15.2021)"
+    },
+
+    {
+        title: "JavaScript is identical to Java?",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+
+    {
+        title: "Which of the following is not a mouse event?",
+        choices: ["onclick", "onmouse", "onbutton", "onmousescroller"],
+        answer: "onmousescroller"
+    }
+];
